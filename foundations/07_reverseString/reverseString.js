7ad0baf981fb9581c75a8dbaaa6c374a972d32d5
terr4ance <lawrence.sheard0@gmail.com>
2026-05-27 22:34:12 +0100

    Finish 07

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    const stringLength = input.length
+    let output = ""
+    for (let index = stringLength; index >= 0; index--) {
+        let character = input.charAt(index)
+        output = (output + character)
+    }
+    return output
 };
 
 // Do not edit below this line
