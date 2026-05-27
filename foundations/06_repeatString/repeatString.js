e5f655d5d7499f8d96be9da00d8082f43132f060
terr4ance <lawrence.sheard0@gmail.com>
2026-05-27 22:24:23 +0100

    Finish 06

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num >= 0) {
+        let newString = ""
+        for (let repeat = 0; repeat < num; repeat++) {
+            newString = (newString+string)
+        }
+        return(newString)
+    } else {
+        return("ERROR")
+    }
 };
 
 // Do not edit below this line
